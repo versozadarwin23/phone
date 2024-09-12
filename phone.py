f6f4e832bfa7d9204c7ea8982765894fbfcf3672
versozadarwin23 <109713017+versozadarwin23@users.noreply.github.com>
2024-09-12 15:17:27 +0800

    Update phone.py

--- phone.py
+++ phone.py
@@ -195,6 +195,7 @@ def device_tasks(device):
         uiautomator2ServerLaunchTimeout='96000',
         platformVersion=device["platformVersion"],
         deviceName=device["deviceName"],
+        unicodeKeyboard=True,
         skipServerInstallation=True,
         ignoreUnimportantViews=True,
         skipDeviceInitialization=True,
@@ -261,7 +262,7 @@ def device_tasks(device):
                 except:
                     pass
             try:
-                WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '[value="Log in"]'))).click()
+                WebDriverWait(driver, 120).until(EC.visibility_of_element_located((By.ID, 'login_password_step_element'))).click()
                 break
             except:
                 pass
